34b87590ef62f4ee796bd081c6d8cef119d6b66a
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-05-05 22:38:33 -0400

    fix: removed broken handleLogout code
    https://github.com/auth0/nextjs-auth0/issues/362

--- apps/nextjs/src/pages/api/auth/[...auth0].ts
+++ apps/nextjs/src/pages/api/auth/[...auth0].ts
@@ -1,19 +1,27 @@
 // pages/api/auth/[...auth0].js
 
 import { type NextApiRequest, type NextApiResponse } from "next";
-import { handleAuth, handleLogout } from "@auth0/nextjs-auth0";
+import { handleAuth } from "@auth0/nextjs-auth0";
 import { z } from "zod";
 
+import { env } from "~/env.mjs";
+
 export default handleAuth({
-  logout: async (req: NextApiRequest, res: NextApiResponse) => {
+  logout: (req: NextApiRequest, res: NextApiResponse) => {
     const {
       query: { returnTo },
     } = req;
     try {
       const returnToParsed = z.string().parse(returnTo);
-      await handleLogout(req, res, {
-        returnTo: returnToParsed,
-      });
+
+      res
+        .redirect(
+          `${env.AUTH0_ISSUER_BASE_URL}/v2/logout?${new URLSearchParams({
+            client_id: env.AUTH0_CLIENT_ID,
+            returnTo: returnToParsed,
+          })}`,
+        )
+        .end();
     } catch (error) {
       console.error(error);
       if (error instanceof Error) {
