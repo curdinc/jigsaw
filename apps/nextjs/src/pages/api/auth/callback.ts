01b0f851f5ea9baadcce7d06c938f10dce233d61
Hans Bhatia <h23bhati@uwaterloo.ca>
2023-05-06 17:45:12 -0400

    chore: edit callback

--- apps/nextjs/src/pages/api/auth/callback.ts
+++ apps/nextjs/src/pages/api/auth/callback.ts
@@ -2,5 +2,6 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("req", req);
+  console.log(req.query);
   res.status(200).redirect("/beta/home");
 }
--- apps/nextjs/src/pages/beta/home.tsx
+++ apps/nextjs/src/pages/beta/home.tsx
@@ -1,4 +1,6 @@
-function home() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
+
+export default withPageAuthRequired(function Home() {
   const logoutLink = `http://localhost:3000/api/auth/logout?returnTo=${encodeURIComponent(
     "http://localhost:3000/beta",
   )}`;
@@ -7,6 +9,4 @@ function home() {
       <a href={logoutLink}>Logout</a>
     </>
   );
-}
-
-export default home;
+});
